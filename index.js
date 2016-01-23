b77b7195653418c6d14d93b744228b31fff19a85
Zachary Smith <billyzacsmith@gmail.com>
2016-01-22 19:42:24 -0700

    Add a Procfile and fix up the port stuff.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var Express = require('express')
 
 var app = Express()
 
+app.set('port', (process.env.PORT || 3030));
+
 app.get('/', function(req, res) {
   res.send('okayeee')
 })
 
-app.listen(3030, console.log('Listening so tight... awww yeah!'))
+app.listen(app.get('port'), console.log('Listening so tight on port ' + app.get('port') + '... awww yeah!'))
