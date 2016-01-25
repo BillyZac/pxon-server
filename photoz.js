113286ae28ec9298783ade59ed1906fc95c1808f
Zachary Smith <billyzacsmith@gmail.com>
2016-01-25 07:22:10 -0700

    Put the pixels into pxon format.

--- photoz.js
+++ photoz.js
@@ -6,23 +6,35 @@ fs.createReadStream('./images/Chuck-Close.png')
         filterType: 4
     }))
     .on('parsed', function() {
+      var pixels = []
       for (var x = 0; x < this.width; x = x+50) {
         for (var y = 0; y < this.height; y = y + 50) {
-          console.log(getPixel(x, y, this))
+          pixels.push(getPixel(x, y, this))
         }
       }
+      console.log(pixels)
     })
 
 function getPixel(x, y, imageData) {
   var data = imageData.data
   var idx = (imageData.width * y + x) << 2
-  var pixel = {
-    x: x,
-    y: y,
-    r: data[idx],
-    g: data[idx+1],
-    b: data[idx+2],
-    a: data[idx+3] / 255 // Is this right?
+
+  var r = data[idx]
+  var g = data[idx+1]
+  var b = data[idx+2]
+  var a = data[idx+3] / 255 // Is this right?
+
+  var color = 'rgba(' +
+    r + ',' +
+    g + ',' +
+    b + ',' +
+    a + ')'
+
+  // Return a pixel in pxon format
+  return {
+    "x": x,
+    "y": y,
+    "color": color,
+    "size": null // Not dealing with size for now
   }
-  return pixel
 }
